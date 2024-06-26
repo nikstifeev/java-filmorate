6b3002b285ebd6207d6f2653d27155b4b337a8ab
nik.stifeev <n.stifeev99@yandex.ru>
2024-06-26 13:48:22 +0500

    sprint10

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,31 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.*;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
 
 /**
  * Film.
  */
-@Getter
-@Setter
+
+@Data
+@Builder
 public class Film {
+    private Long id;
+
+    @NotBlank(message = "название не может быть пустым")
+    private String name;
+
+    @Size(max = 200, message = "максимальная длина описания не больше 200 символов")
+    private String description;
+
+    @NotNull(message = "дата не может быть null")
+    @PastOrPresent(message = "дата релиза не может быть в будущем")
+    private LocalDate releaseDate;
+
+    @NotNull(message = "продолжительность не может быть null")
+    @Positive(message = "продолжительность фильма должна быть положительным числом")
+    private Long duration;
 }
